24047e3dce94c89fa3d58a55d5308dc7de93e7b8
Jibdolti <154307791+Jibdolti@users.noreply.github.com>
2024-09-30 15:39:18 +0900

    update0930

--- script/action.js
+++ script/action.js
@@ -7,8 +7,6 @@ function randomizeStartPosition() {
 
 window.addEventListener('load', randomizeStartPosition);
 
-window.addEventListener('load', randomizeStartPosition);
-
   // 확대/축소 방지 코드
   document.addEventListener('wheel', function(event) {
     if (event.ctrlKey) {
@@ -24,3 +22,21 @@ document.addEventListener('keydown', function(event) {
 
 // 페이지 로드 시 기본 배율로 설정
 document.body.style.zoom = "100%"; // 일부 브라우저에서 배율을 강제 설정
+
+
+$(window).scroll(function(){
+    let scrT = $(window).scrollTop();
+    // $('.star').css({})
+      // 나누기 연산으로 스크롤 단계 결정 (1000px씩)
+      var phase = Math.floor(scrT / 1000);
+      
+      // 각도 계산
+      var angle = scrT % 1000;
+
+      // 시계 방향 회전 또는 반시계 방향 회전
+      if (phase % 2 === 0) {
+        $('.star0 .left').css('transform', 'rotate(' + angle + 'deg)');
+      } else {
+        $('.star0 .left').css('transform', 'rotate(-' + angle + 'deg)');
+      }
+})
